7482120482605d8518cbc5fc478336839c7f60f4
sbadia <sbadia@cimne.upc.edu>
2015-12-08 18:13:47 +0100

    Bug-fixed FE machinery for high-order methods. There was a hard-to-find error in the fe_map update, the coordinates were the ones of the gp instead of the corners... I have checked results for a silly Poisson problem up to order 4

--- Sources/Serial/NewIntegration/SB_discrete_integration.f90
+++ Sources/Serial/NewIntegration/SB_discrete_integration.f90
@@ -64,100 +64,6 @@ end subroutine impose_strong_dirichlet_data
 
 end module SB_discrete_integration_names
 
-!%%%%%%%%%%%%%%%%%%%%%%%
-
-! module poisson_discrete_integration_names
-!   use shape_values_names
-!   use SB_discrete_integration_names
-!   use reference_fe_names
-!   use types_names
-!   implicit none
-! # include "debug.i90"
-!   private
-!   type, extends(SB_discrete_integration_t) :: poisson_discrete_integration_t
-!   integer(ip) :: viscosity
-! contains
-!   procedure :: integrate
-! end type poisson_discrete_integration_t
-
-! public :: poisson_discrete_integration_t
-
-! contains
-
-!   subroutine integrate (this, vol_int, elmat, elvec)
-!     implicit none
-!     class(poisson_discrete_integration_t), intent(inout) :: this
-!     type(SB_volume_integrator_t)    , intent(inout) :: vol_int
-!     real(rp), intent(inout) :: elmat(:,:), elvec(:)
-
-!     integer(ip)  :: ndime
-!     real(rp)     :: dtinv, c1, c2, mu
-
-!     integer(ip)  :: idime,igaus,inode,jnode,ngaus,nnode
-!     real(rp) :: factor
-
-!     type(SB_quadrature_t), pointer :: quad
-!     type(SB_interpolation_t), pointer :: int
-!     class(reference_fe_t), pointer :: ref_fe
-!     type(fe_map_t), pointer :: fe_map
-
-!     type(shape_values_t), pointer :: shape_gradient_test, shape_gradient_trial
-
-!     real(rp), pointer :: grad_test(:,:), grad_trial(:,:)
-
-!     integer(ip) :: a, b
-
-!     quad => vol_int%get_quadrature()
-!     int => vol_int%get_interpolation()
-!     ref_fe => vol_int%get_reference_fe()
-!     fe_map => vol_int%get_fe_map()
-
-
-!     nnode = ref_fe%get_number_nodes()
-!     ngaus = quad%get_number_integration_points()
-!     ndime = ref_fe%get_number_dimensions()
-
-!     shape_gradient_test => vol_int%get_gradients()
-!     shape_gradient_trial => vol_int%get_gradients()
-
-!     do igaus = 1,ngaus
-!        factor = fe_map%get_det_jacobian(igaus) * quad%get_weight(igaus)
-!        do inode = 1, nnode
-!           grad_trial => shape_gradient_trial%get_value(inode,igaus)
-!           do jnode = 1, nnode
-!              grad_test => shape_gradient_test%get_value(jnode,igaus)
-!              ! elmat = elmat + grad_trial*grad_test
-!              do a = 1,size(shape_gradient_test%get_value(inode,igaus),1)
-!                 do b= 1,size(shape_gradient_test%get_value(inode,igaus),2)
-!                    elmat(inode,jnode) = elmat(inode,jnode) &
-!                         + factor * grad_test(a,b) * grad_trial(a,b) 
-!                 end do
-!              end do
-
-!           end do
-!        end do
-!     end do
-
-!  !write (*,*) 'XXXXXXXXX ELMAT XXXXXXXXX'
-!  !write (*,*) elmat
-
-
-!  ! Apply boundary conditions
-!  !call impose_strong_dirichlet_data(fe) 
-
-! end subroutine integrate
-
-
-! end module poisson_discrete_integration_names
-
-
-
-!%%%%%%%%%%%%%%%%%%%%%%%
-
-
-
-
-
 module poisson_discrete_integration_names
   use shape_values_names
   use SB_assembler_names
@@ -242,9 +148,14 @@ contains
 
        fe => fe_space%get_fe(ielem)
        call fe%update_integration( number_fe_spaces )
+       quad => fe%get_quadrature()
        fe_map => fe%get_fe_map()
        vol_int => fe%get_volume_integrator()
 
+       call fe_map%print()
+       call quad%print()
+       !call vol_int(1)%p%print()
+       !check(0==1)
        !gcall vol_int%print()
 
        call fe%get_number_nodes_field( number_nodes, number_fe_spaces )
@@ -257,7 +168,6 @@ contains
        !write(*,*) 'bc_value',bc_value
        !write(*,*) 'number_nodes',number_nodes
 
-       quad => fe%get_quadrature()
        ngaus = quad%get_number_integration_points()
        !write(*,*) 'ngaus',ngaus
 
