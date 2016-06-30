bce23403ea5f6f423afefd3f851d4af403f29e49
vsande <vsande@cimne.upc.edu>
2016-06-30 17:51:43 +0200

    New branch from iterator.  - Added SISL external proyect  - Link drivers agains SISL library and include dirs  - Create "Soures/Driver/Serial/principe" directory with    the SISL examples provided by @principe

---
+++ Sources/Drivers/Serial/principe/test_sisl/types.f90
@@ -0,0 +1,9 @@
+module types
+!-----------------------------------------------------------------------
+!    This module contains kind and type definitions.
+!-----------------------------------------------------------------------
+  implicit none
+  integer, parameter       :: ip  = 4    ! Integer precision
+  integer, parameter       :: rp  = 8    ! Real precision
+
+end module types
