8c1835c932db81fa8882f2a0628b4531ad1a4548
amartin <amartin@cimne.upc.edu>
2016-07-26 13:24:20 +0200

    - Further developments towards having a functional type(new_serial_fe_space_t).   I could already assemble an scalar Laplacian problem with homogeneous Neumann BCs,   and validated it against the one obtained with type(serial_fe_space_t).

--- Sources/Drivers/Serial/Examples/test_new_serial_fe_space/poisson_discrete_integration.f90
+++ Sources/Drivers/Serial/Examples/test_new_serial_fe_space/poisson_discrete_integration.f90
@@ -119,70 +119,86 @@ contains
     class(new_serial_fe_space_t)         , intent(inout) :: fe_space
     class(matrix_array_assembler_t)      , intent(inout) :: matrix_array_assembler
 
-    !type(finite_element_t), pointer :: fe
-    !type(volume_integrator_t), pointer :: vol_int
-    !real(rp), allocatable :: elmat(:,:), elvec(:)
-    !type(fe_map_t), pointer :: fe_map
-    !type(quadrature_t), pointer :: quad
-
-    !integer(ip)  :: igaus,inode,jnode,ngaus
-    !real(rp)     :: factor
-
-    !type(vector_field_t) :: grad_test, grad_trial
-
-    !integer(ip) :: number_fe_spaces
-
-    !integer(ip), pointer :: field_blocks(:)
-    !logical, pointer :: field_coupling(:,:)
-
-    !integer(ip) :: ielem, iapprox, number_nodes
-    !type(i1p_t), pointer :: elem2dof(:)
-    !integer(ip), allocatable :: number_nodes_per_field(:)  
-
-    !number_fe_spaces = fe_space%get_number_fe_spaces()
-    !field_blocks => fe_space%get_field_blocks()
-    !field_coupling => fe_space%get_field_coupling()
-
-    !fe => fe_space%get_finite_element(1)
-    !number_nodes = fe%get_number_nodes()
-    !call memalloc ( number_nodes, number_nodes, elmat, __FILE__, __LINE__ )
-    !call memalloc ( number_nodes, elvec, __FILE__, __LINE__ )
-    !call memalloc ( number_fe_spaces, number_nodes_per_field, __FILE__, __LINE__ )
-    !call fe%get_number_nodes_per_field( number_nodes_per_field )
-
-    !call fe_space%initialize_integration()
+    ! FE space traversal-related data types
+    type(fe_iterator_t) :: fe_iterator
+    type(fe_accessor_t) :: fe
     
-    !quad => fe%get_quadrature()
-    !ngaus = quad%get_number_quadrature_points()
-    !do ielem = 1, fe_space%get_number_elements()
-    !   elmat = 0.0_rp
-    !   elvec = 0.0_rp
+    ! FE integration-related data types
+    type(fe_map_t)           , pointer :: fe_map
+    type(quadrature_t)       , pointer :: quad
+    type(volume_integrator_t), pointer :: vol_int
+    type(vector_field_t)               :: grad_test, grad_trial
+    
+    ! FE matrix and vector i.e., A_K + f_K
+    real(rp), allocatable              :: elmat(:,:), elvec(:)
 
-    !   fe => fe_space%get_finite_element(ielem)
-    !   call fe%update_integration()
-    !   
-    !   fe_map   => fe%get_fe_map()
-    !   vol_int  => fe%get_volume_integrator(1)
-    !   elem2dof => fe%get_elem2dof()
+    integer(ip)  :: istat
+    integer(ip)  :: qpoint, num_quad_points
+    integer(ip)  :: idof, jdof, num_dofs
+    real(rp)     :: factor
 
-    !   do igaus = 1,ngaus
-    !      factor = fe_map%get_det_jacobian(igaus) * quad%get_weight(igaus)
-    !      do inode = 1, number_nodes
-    !         call vol_int%get_gradient(inode,igaus,grad_trial)
-    !         do jnode = 1, number_nodes
-    !            call vol_int%get_gradient(jnode,igaus,grad_test)
-    !            elmat(inode,jnode) = elmat(inode,jnode) + factor * grad_test * grad_trial
-    !         end do
-    !      end do
-    !   end do
-    !   
-    !   ! Apply boundary conditions
-    !   call fe%impose_strong_dirichlet_bcs( elmat, elvec )
-    !   call matrix_array_assembler%assembly( number_fe_spaces, number_nodes_per_field, elem2dof, field_blocks,  field_coupling, elmat, elvec )
-    !end do
-    !call memfree ( number_nodes_per_field, __FILE__, __LINE__ )
-    !call memfree ( elmat, __FILE__, __LINE__ )
-    !call memfree ( elvec, __FILE__, __LINE__ )
+    integer(ip)  :: number_fields
+
+    integer(ip), pointer :: field_blocks(:)
+    logical    , pointer :: field_coupling(:,:)
+
+    type(i1p_t), allocatable :: elem2dof(:)
+    integer(ip), allocatable :: num_dofs_per_field(:)  
+
+    
+    number_fields = fe_space%get_number_fields()
+    allocate( elem2dof(number_fields), stat=istat); check(istat==0);
+    field_blocks => fe_space%get_field_blocks()
+    field_coupling => fe_space%get_field_coupling()
+    
+    fe_iterator = fe_space%create_fe_iterator()
+    call fe_space%initialize_fe_integration()
+    
+    call fe_iterator%current(fe)
+    num_dofs = fe%get_number_dofs()
+    call memalloc ( num_dofs, num_dofs, elmat, __FILE__, __LINE__ )
+    call memalloc ( num_dofs, elvec, __FILE__, __LINE__ )
+    call memalloc ( number_fields, num_dofs_per_field, __FILE__, __LINE__ )
+    call fe%get_number_dofs_per_field(num_dofs_per_field)
+    quad            => fe%get_quadrature()
+    num_quad_points = quad%get_number_quadrature_points()
+    fe_map          => fe%get_fe_map()
+    vol_int         => fe%get_volume_integrator(1)
+    
+    do while ( .not. fe_iterator%has_finished() )
+       ! Get current FE
+       call fe_iterator%current(fe)
+       
+       ! Update FE-integration related data structures
+       call fe%update_integration()
+       
+       ! Get DoF numbering within current FE
+       call fe%get_elem2dof(elem2dof)
+
+       ! Compute element matrix and vector
+       elmat = 0.0_rp
+       elvec = 0.0_rp
+       do qpoint = 1, num_quad_points
+          factor = fe_map%get_det_jacobian(qpoint) * quad%get_weight(qpoint)
+          do idof = 1, num_dofs
+             call vol_int%get_gradient(idof, qpoint, grad_trial)
+             do jdof = 1, num_dofs
+                call vol_int%get_gradient(jdof, qpoint, grad_test)
+                ! A_K(i,j) = (grad(phi_i),grad(phi_j))
+                elmat(idof,jdof) = elmat(idof,jdof) + factor * grad_test * grad_trial
+             end do
+          end do
+       end do
+       
+       ! Apply boundary conditions (IMPLEMENTATION PENDING)
+       ! call fe%impose_strong_dirichlet_bcs( elmat, elvec )
+       call matrix_array_assembler%assembly( number_fields, num_dofs_per_field, elem2dof, field_blocks, field_coupling, elmat, elvec )
+       call fe_iterator%next()
+    end do
+    deallocate (elem2dof, stat=istat); check(istat==0);
+    call memfree ( num_dofs_per_field, __FILE__, __LINE__ )
+    call memfree ( elmat, __FILE__, __LINE__ )
+    call memfree ( elvec, __FILE__, __LINE__ )
   end subroutine integrate_new_serial_fe_space
   
 end module poisson_discrete_integration_names
