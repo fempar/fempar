7cbf781b41bf1aa64697beb76e46eddc29d2e00b
eneiva <eneiva@cimne.upc.edu>
2016-08-18 11:21:39 +0200

    Minor changes to clean the code

--- Sources/Drivers/Par/Examples/par_test_poisson/poisson_discrete_integration.f90
+++ Sources/Drivers/Par/Examples/par_test_poisson/poisson_discrete_integration.f90
@@ -61,7 +61,7 @@ contains
 
     integer(ip)  :: istat
     integer(ip)  :: qpoint, num_quad_points
-    integer(ip)  :: idof, jdof, number_nodes
+    integer(ip)  :: idof, jdof, num_dofs
     real(rp)     :: factor
 
     integer(ip)  :: number_fields
