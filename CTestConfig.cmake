6d8e8043388de63613708d58b78c48e29050c901
vsande <vsande@cimne.upc.edu>
2017-01-12 12:41:28 +0100

    - Remove git commit from build name - Point to my.cdash.org testing dashboard - Ctest update step do not perform any change in sources, only is   aware of the current commit version

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -7,10 +7,12 @@
 ##   INCLUDE(CTest)
 
 set(CTEST_PROJECT_NAME "fempar")
-set(CTEST_NIGHTLY_START_TIME "0")
+set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "servercomfus:8080")
-set(CTEST_DROP_LOCATION "/submit.php?project=fempar")
+set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=Fempar")
 set(CTEST_DROP_SITE_CDASH TRUE)
+set(CTEST_UPDATE_VERSION_ONLY TRUE)
+
 
