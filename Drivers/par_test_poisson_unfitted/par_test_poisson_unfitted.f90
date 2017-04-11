6973c9fee61aeb4ee0543ce8ebd701ad441e53b5
fverdugo <fverdugo@cimne.upc.edu>
2017-04-11 16:49:53 +0200

    Copied `par_test_poisson_void_fe` and renamed into `par_test_poisson_unfitted` as the starting point of the parallel driver for unditted FEs

---
+++ Drivers/par_test_poisson_unfitted/par_test_poisson_unfitted.f90
@@ -0,0 +1,17 @@
+program par_test_poisson_unfitted
+  use fempar_names
+  use par_test_poisson_unfitted_driver_names
+  !$ use omp_lib
+  implicit none
+  type(par_test_poisson_unfitted_fe_driver_t), save :: test_driver 
+  !$OMP THREADPRIVATE(test_driver)
+
+  !call sleep(20)
+  !$OMP PARALLEL 
+  !$ write(*,*) 'Begining with',omp_get_num_threads(),'threads'
+  !$OMP BARRIER
+  call fempar_init()  
+  call test_driver%run_simulation()
+  call fempar_finalize()
+  !$OMP END PARALLEL   
+end program par_test_poisson_unfitted
