c520e86b8ccfe35fc3374318386269ebe622e5d5
amartin <amartin@cimne.upc.edu>
2017-05-15 18:19:05 +0200

    Added test_std_vector to set of Tests to be performed

---
+++ Sources/Tests/Serial/test_std_vector/test_std_vector.f90
@@ -0,0 +1,69 @@
+! Copyright (C) 2014 Santiago Badia, Alberto F. Martín and Javier Principe
+!
+! This file is part of FEMPAR (Finite Element Multiphysics PARallel library)
+!
+! FEMPAR is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! FEMPAR is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with FEMPAR. If not, see <http://www.gnu.org/licenses/>.
+!
+! Additional permission under GNU GPL version 3 section 7
+!
+! If you modify this Program, or any covered work, by linking or combining it 
+! with the Intel Math Kernel Library and/or the Watson Sparse Matrix Package 
+! and/or the HSL Mathematical Software Library (or a modified version of them), 
+! containing parts covered by the terms of their respective licenses, the
+! licensors of this Program grant you additional permission to convey the 
+! resulting work. 
+!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#include "debug.i90"
+program test_std_vector
+  use fempar_names
+  implicit none
+  type(std_vector_integer_ip_t) :: v
+  integer(ip) :: i
+  
+  call fempar_init()
+  !Set some initial content:
+  do i=1, 100
+    call v%push_back(i)
+    write(*,'(a,i4,a,i4)') 'Size=', v%size(), ' Capacity=', v%capacity() 
+    check ( v%get(i) == i )
+  end do
+  
+  call v%resize(1000,34)
+  do i=1, 100
+    check ( v%get(i) == i )
+  end do
+  do i=101,1000
+    check ( v%get(i) == 34 )
+  end do
+  
+  call v%resize(38)
+  do i=1, v%size()
+    call v%set(i,-i)
+    write(*,'(a,i4,a,i4)') 'Size=', v%size(), ' Capacity=', v%capacity() 
+    check ( v%get(i) == -i )
+  end do
+  
+  call v%erase(10)
+  do i=1, 9
+    check ( v%get(i) == i )
+  end do
+  
+  do i=10, v%size()
+    check ( v%get(i) == -(i+1) )
+  end do
+   
+  call v%free()
+  call fempar_finalize()
+end program test_std_vector
