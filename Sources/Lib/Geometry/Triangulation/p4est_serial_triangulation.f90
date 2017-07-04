31e46a3e85759e1f801a20f9ef411d0695b9a6df
eneiva <eneiva@cimne.upc.edu>
2017-07-04 10:18:03 +0200

    [serial_p4est_triangulation] Added p4est_cell_iterator_is_ancestor
     * This procedure gives support to the FE activation of h-adaptive meshes.
     * (!) It relies on the dim-dependent parameter `max_allowed_refinement_levels_2D`. The
       treatment of this parameter and the procedure should be rethought in a cleaner way.

--- Sources/Lib/Geometry/Triangulation/p4est_serial_triangulation.f90
+++ Sources/Lib/Geometry/Triangulation/p4est_serial_triangulation.f90
@@ -75,6 +75,8 @@ module p4est_serial_triangulation_names
   integer(ip), parameter :: P4EST_2_FEMPAR_CORNER(NUM_CORNERS_2D) = [ 1, 2, 3, 4 ]
   integer(ip), parameter :: P4EST_2_FEMPAR_FACE  (NUM_FACES_2D)   = [ 3, 4, 1, 2 ]
   
+  integer(ip), parameter :: max_allowed_refinement_levels_2D = 29
+  
   integer(ip), parameter :: refinement = 1 
   integer(ip), parameter :: coarsening = -1 
   integer(ip), parameter :: do_nothing = 0 
@@ -117,6 +119,8 @@ module p4est_serial_triangulation_names
     !procedure, non_overridable           :: get_vef                 => p4est_cell_iterator_get_vef
     procedure                            :: is_local                => p4est_cell_iterator_is_local
     procedure                            :: is_ghost                => p4est_cell_iterator_is_ghost
+    
+    procedure                            :: is_ancestor             => p4est_cell_iterator_is_ancestor
     
     procedure                            :: set_for_coarsening      => p4est_cell_iterator_set_for_coarsening
     procedure                            :: set_for_refinement      => p4est_cell_iterator_set_for_refinement
@@ -262,6 +266,8 @@ module p4est_serial_triangulation_names
     procedure, non_overridable :: not_enabled_error => p4est_serial_triangulation_not_enabled_error
 #endif
   end type p4est_serial_triangulation_t
+  
+  public :: max_allowed_refinement_levels_2D
   
   public :: p4est_serial_triangulation_t, p4est_cell_iterator_t, p4est_vef_iterator_t
   public :: refinement, coarsening, do_nothing
