0dcd8f8ee476ebcd0c69f4328de8e6331fadc36b
sbadia <sbadia@cimne.upc.edu>
2017-08-22 18:34:38 +0200

    renaming.sh num_fixed_dof_values num_fixed_dofs

--- Sources/Lib/Integration/FESpace/fe_space.f90
+++ Sources/Lib/Integration/FESpace/fe_space.f90
@@ -447,7 +447,7 @@ module fe_space_names
      
      ! Strong Dirichlet BCs-related member variables
      class(conditions_t)           , pointer     :: conditions    => NULL()
-     integer(ip)                                 :: num_fixed_dof_values
+     integer(ip)                                 :: num_fixed_dofs
      logical                       , allocatable :: at_strong_dirichlet_boundary_x_fe(:,:)
      
      ! Descriptor of the block layout selected for the PDE system at hand
@@ -555,7 +555,7 @@ module fe_space_names
      procedure                           :: get_environment                              => serial_fe_space_get_environment
      procedure, non_overridable          :: get_conditions                               => serial_fe_space_get_conditions
      procedure, non_overridable          :: set_conditions                               => serial_fe_space_set_conditions
-     procedure                           :: get_num_fixed_dof_values             => serial_fe_space_get_num_fixed_dof_values
+     procedure                           :: get_num_fixed_dofs             => serial_fe_space_get_num_fixed_dofs
      procedure                           :: get_num_blocks                            => serial_fe_space_get_num_blocks
      procedure                           :: get_field_blocks                             => serial_fe_space_get_field_blocks
      procedure                           :: get_field_coupling                           => serial_fe_space_get_field_coupling
