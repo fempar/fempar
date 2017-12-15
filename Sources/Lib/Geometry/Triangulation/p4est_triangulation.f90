227f85b7a17bec8bd51d6d1078a98009bdb8ab0e
amartin <amartin@cimne.upc.edu>
2017-12-15 10:53:54 +0100

    Bug-fixed test errors in serial h-adaptive tests

--- Sources/Lib/Geometry/Triangulation/p4est_triangulation.f90
+++ Sources/Lib/Geometry/Triangulation/p4est_triangulation.f90
@@ -35,6 +35,7 @@ module p4est_triangulation_names
   use environment_names
   use execution_context_names
   use mpi_context_names
+  use serial_context_names
   use uniform_hex_mesh_generator_names
   use p4est_bindings_names
   use reference_fe_names
