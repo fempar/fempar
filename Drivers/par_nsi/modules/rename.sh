a1018a87576a6628b9a9f2f4d482eb1e85605290
amartin <amartin@cimne.upc.edu>
2018-01-21 08:31:46 +0100

    Remove par_nsi from Drivers folder

--- Drivers/par_nsi/modules/rename.sh
+++
@@ -1,6 +0,0 @@
-before=$1
-after=$2
-
-cd ~/git-repos/fempar/Drivers/par_nsi/modules
-
-for i in $(ls); do j=$(cat $i |  grep $before );  if [ ! "$j" == "" ]; then cat $i | sed s/"$before"/"$after"/g > /tmp/kk; cp /tmp/kk $i; fi; done;
