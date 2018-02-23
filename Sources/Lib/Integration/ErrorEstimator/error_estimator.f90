d3fc178f1126aa95ca99258dffd9cdd944dd759c
eneiva <eneiva@cimne.upc.edu>
2018-02-23 19:16:24 +0100

    [refinement_strategy] Introduced new type error_objective_refinement_strategy_t
     * implemented this refinement strategy for serial environment

--- Sources/Lib/Integration/ErrorEstimator/error_estimator.f90
+++ Sources/Lib/Integration/ErrorEstimator/error_estimator.f90
@@ -36,70 +36,70 @@ module error_estimator_names
   private
   
   
-  type, abstract :: explicit_error_estimator_t
-    class(serial_fe_space_t)    , pointer     :: fe_space
-    !class(refinement_strategy_t), allocatable :: refinement_strategy
-    type(std_vector_real_rp_t)  , allocatable :: local_estimates
-    real(rp)                                  :: global_estimate
-    type(std_vector_real_rp_t)  , allocatable :: local_true_errors
-    real(rp)                                  :: global_true_error
-    type(std_vector_real_rp_t)  , allocatable :: local_effectivities
-    real(rp)                                  :: global_effectivity
-    integer(ip)                               :: output_mode
-    integer(ip)                               :: logical_unit
+  type, abstract :: error_estimator_t
+    class(serial_fe_space_t)         , pointer     :: fe_space => NULL()
+    type(std_vector_real_rp_t)       , allocatable :: local_estimates
+    real(rp)                                       :: global_estimate
+    type(std_vector_real_rp_t)       , allocatable :: local_true_errors
+    real(rp)                                       :: global_true_error
+    type(std_vector_real_rp_t)       , allocatable :: local_effectivities
+    real(rp)                                       :: global_effectivity
+    integer(ip)                                    :: output_mode
+    integer(ip)                                    :: logical_unit
    contains
     procedure (compute_local_estimates_interface)  , deferred :: compute_local_estimates
     procedure (compute_local_true_errors_interface), deferred :: compute_local_true_errors
     procedure                           :: create                           => eee_create
     procedure                           :: free                             => eee_free
-    !procedure, non_overridable          :: setup_adaptive_strategy          => eee_setup_adaptive_strategy
-    !procedure, non_overridable          :: has_finished_refinement          => eee_has_finished_adaptive_refinement
-    !procedure, non_overridable          :: update_refinement_flags          => eee_update_refinement_flags
+    !procedure, non_overridable          :: setup_adaptive_strategy          => eee_setup_adaptive_strategys
     !procedure, non_overridable          :: compute_global_estimate          => eee_compute_global_estimate
     !procedure, non_overridable          :: compute_global_true_error        => eee_compute_global_true_error
     !procedure, non_overridable          :: compute_local_effectivities      => eee_get_local_effectivities
     !procedure, non_overridable          :: compute_global_effectivity       => eee_get_global_effectivity
     procedure, non_overridable          :: get_local_estimates              => eee_get_local_estimates
+    procedure, non_overridable          :: get_local_estimate_entries       => eee_get_local_estimate_entries
     procedure, non_overridable          :: get_global_estimate              => eee_get_global_estimate
     procedure, non_overridable          :: get_local_true_errors            => eee_get_local_true_errors
+    procedure, non_overridable          :: get_local_true_error_entries     => eee_get_local_true_error_entries
     procedure, non_overridable          :: get_global_true_error            => eee_get_global_true_error
     procedure, non_overridable          :: get_local_effectivities          => eee_get_local_effectivities
+    procedure, non_overridable          :: get_local_effectivity_entries    => eee_get_local_effectivity_entries
     procedure, non_overridable          :: get_global_effectivity           => eee_get_global_effectivity
     !procedure, non_overridable          :: print_estimator_history_header   => eee_print_estimator_history_header
     !procedure, non_overridable          :: print_estimator_history_new_line => eee_print_estimator_history_new_line
     !procedure, non_overridable          :: print_estimator_history_footer   => eee_print_estimator_history_footer
-  end type explicit_error_estimator_t
+  end type error_estimator_t
   
   abstract interface
 
     subroutine compute_local_estimates_interface(this)
-      import :: explicit_error_estimator_t
-      class(explicit_error_estimator_t), intent(inout) :: this
+      import :: error_estimator_t
+      class(error_estimator_t), intent(inout) :: this
     end subroutine compute_local_estimates_interface
 
     subroutine compute_local_true_errors_interface(this)
-      import :: explicit_error_estimator_t
-      class(explicit_error_estimator_t), intent(inout) :: this
+      import :: error_estimator_t
+      class(error_estimator_t), intent(inout) :: this
     end subroutine compute_local_true_errors_interface
 
   end interface
   
-  public :: explicit_error_estimator_t
+  public :: error_estimator_t
 
 contains
 
  subroutine eee_create ( this, fe_space, parameter_list )
    implicit none
-   class(explicit_error_estimator_t)        , intent(inout) :: this
-   class(serial_fe_space_t)         , target, intent(in)    :: fe_space
-   type(parameterlist_t)                    , intent(in)    :: parameter_list
+   class(error_estimator_t)        , intent(inout) :: this
+   class(serial_fe_space_t), target, intent(in)    :: fe_space
+   type(parameterlist_t)           , intent(in)    :: parameter_list
    call this%free()
    this%fe_space => fe_space
  end subroutine eee_create
 
  subroutine eee_free ( this )
    implicit none
-   class(explicit_error_estimator_t), intent(inout) :: this
+   class(error_estimator_t), intent(inout) :: this
    nullify(this%fe_space)
    this%output_mode        = 0_ip
    this%logical_unit       = 0_ip
@@ -113,42 +113,63 @@ contains
  
  function eee_get_local_estimates ( this )
    implicit none
-   class(explicit_error_estimator_t), target, intent(inout) :: this
+   class(error_estimator_t), target, intent(inout) :: this
    type(std_vector_real_rp_t), pointer :: eee_get_local_estimates
    eee_get_local_estimates => this%local_estimates
  end function eee_get_local_estimates
  
+ function eee_get_local_estimate_entries ( this )
+   implicit none
+   class(error_estimator_t), target, intent(inout) :: this
+   real(rp), pointer :: eee_get_local_estimate_entries(:)
+   eee_get_local_estimate_entries => this%local_estimates%get_pointer()
+ end function eee_get_local_estimate_entries
+ 
  function eee_get_global_estimate ( this )
    implicit none
-   class(explicit_error_estimator_t), intent(inout) :: this
+   class(error_estimator_t), intent(inout) :: this
    real(rp) :: eee_get_global_estimate
    eee_get_global_estimate = this%global_estimate
  end function eee_get_global_estimate
  
  function eee_get_local_true_errors ( this )
    implicit none
-   class(explicit_error_estimator_t), target, intent(inout) :: this
+   class(error_estimator_t), target, intent(inout) :: this
    type(std_vector_real_rp_t), pointer :: eee_get_local_true_errors
    eee_get_local_true_errors => this%local_true_errors
  end function eee_get_local_true_errors
  
+ function eee_get_local_true_error_entries ( this )
+   implicit none
+   class(error_estimator_t), target, intent(inout) :: this
+   real(rp), pointer :: eee_get_local_true_error_entries(:)
+   eee_get_local_true_error_entries => this%local_true_errors%get_pointer()
+ end function eee_get_local_true_error_entries
+ 
  function eee_get_global_true_error ( this )
    implicit none
-   class(explicit_error_estimator_t), intent(inout) :: this
+   class(error_estimator_t), intent(inout) :: this
    real(rp) :: eee_get_global_true_error
    eee_get_global_true_error = this%global_true_error
  end function eee_get_global_true_error
  
  function eee_get_local_effectivities ( this )
    implicit none
-   class(explicit_error_estimator_t), target, intent(inout) :: this
+   class(error_estimator_t), target, intent(inout) :: this
    type(std_vector_real_rp_t), pointer :: eee_get_local_effectivities
    eee_get_local_effectivities => this%local_effectivities
  end function eee_get_local_effectivities
  
+ function eee_get_local_effectivity_entries ( this )
+   implicit none
+   class(error_estimator_t), target, intent(inout) :: this
+   real(rp), pointer :: eee_get_local_effectivity_entries(:)
+   eee_get_local_effectivity_entries => this%local_effectivities%get_pointer()
+ end function eee_get_local_effectivity_entries
+ 
  function eee_get_global_effectivity ( this )
    implicit none
-   class(explicit_error_estimator_t), intent(inout) :: this
+   class(error_estimator_t), intent(inout) :: this
    real(rp) :: eee_get_global_effectivity
    eee_get_global_effectivity = this%global_effectivity
  end function eee_get_global_effectivity
---
+++ Sources/Lib/Integration/ErrorEstimator/refinement_strategy.f90
@@ -0,0 +1,128 @@
+! Copyright (C) 2014 Santiago Badia, Alberto F. Martín and Javier Principe
+!
+! This file is part of FEMPAR (Finite Element Multiphysics PARallel library)
+!
+! FEMPAR is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! FEMPAR is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with FEMPAR. If not, see <http://www.gnu.org/licenses/>.
+!
+! Additional permission under GNU GPL version 3 section 7
+!
+! If you modify this Program, or any covered work, by linking or combining it 
+! with the Intel Math Kernel Library and/or the Watson Sparse Matrix Package 
+! and/or the HSL Mathematical Software Library (or a modified version of them), 
+! containing parts covered by the terms of their respective licenses, the
+! licensors of this Program grant you additional permission to convey the 
+! resulting work. 
+!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+module refinement_strategy_names
+  use types_names
+  use fe_space_names
+  use p4est_triangulation_names
+  use std_vector_real_rp_names
+  use error_estimator_names
+  use FPL
+  
+  implicit none
+# include "debug.i90"
+  private
+  
+  character(len=*), parameter :: error_objective_key         = 'error_objective'
+  character(len=*), parameter :: objective_tolerance_key     = 'objective_tolerance'
+  character(len=*), parameter :: max_num_mesh_iterations_key = 'max_num_mesh_iterations'
+  
+  type :: error_objective_refinement_strategy_t
+    private
+    class(error_estimator_t), pointer :: error_estimator
+    real(rp)                          :: error_objective
+    real(rp)                          :: objective_tolerance
+    integer(ip)                       :: current_mesh_iteration
+    integer(ip)                       :: max_num_mesh_iterations
+   contains
+    procedure :: create                  => eors_create
+    procedure :: update_refinement_flags => eors_update_refinement_flags
+    procedure :: has_finished_refinement => eors_has_finished_refinement
+  end type error_objective_refinement_strategy_t
+  
+  public :: error_objective_refinement_strategy_t
+  
+  public :: error_objective_key, objective_tolerance_key, max_num_mesh_iterations_key
+  
+contains
+  
+  subroutine eors_create(this,error_estimator,parameter_list)
+    class(error_objective_refinement_strategy_t)         , intent(inout) :: this
+    class(error_estimator_t)                    , target , intent(in)    :: error_estimator
+    type(parameterlist_t)                                , intent(in)    :: parameter_list
+    integer(ip) :: FPLerror
+    this%error_estimator => error_estimator
+    assert(parameter_list%isPresent(error_objective_key))
+    assert(parameter_list%isAssignable(error_objective_key,this%error_objective))
+    FPLerror = parameter_list%get(key = error_objective_key, value = this%error_objective)
+    assert(FPLerror==0)
+    assert(parameter_list%isPresent(objective_tolerance_key))
+    assert(parameter_list%isAssignable(objective_tolerance_key,this%objective_tolerance))
+    FPLerror = parameter_list%get(key = objective_tolerance_key, value = this%objective_tolerance)
+    assert(FPLerror==0)
+    if ( parameter_list%isPresent(max_num_mesh_iterations_key) ) then
+      FPLerror = parameter_list%get(key = max_num_mesh_iterations_key, value = this%max_num_mesh_iterations)
+      assert(FPLerror==0)
+    else
+      this%max_num_mesh_iterations = 100
+    end if
+    this%current_mesh_iteration = 0
+  end subroutine eors_create
+  
+  subroutine eors_update_refinement_flags(this,refinement_and_coarsening_flags)
+    class(error_objective_refinement_strategy_t), intent(inout) :: this
+    type(std_vector_real_rp_t)                  , intent(inout) :: refinement_and_coarsening_flags
+    real(rp)   , pointer :: refinement_and_coarsening_flags_entries(:)
+    integer(ip)          :: i, num_of_entries
+    real(rp)   , pointer :: local_estimate_entries(:)
+    real(rp)             :: error_upper_bound, error_lower_bound
+    refinement_and_coarsening_flags_entries => refinement_and_coarsening_flags%get_pointer()
+    num_of_entries     = size(local_estimate_entries)
+    local_estimate_entries => this%error_estimator%get_local_estimate_entries()
+    error_upper_bound  = this%error_objective * ( 1.0_rp + this%objective_tolerance )
+    error_lower_bound  = this%error_objective * ( 1.0_rp - this%objective_tolerance )
+    do i = 1, num_of_entries
+      if ( local_estimate_entries(i) > error_upper_bound ) then
+        refinement_and_coarsening_flags_entries(i) = refinement
+      else if ( local_estimate_entries(i) < error_lower_bound ) then
+        refinement_and_coarsening_flags_entries(i) = coarsening
+      else
+        refinement_and_coarsening_flags_entries(i) = do_nothing
+      end if
+    end do
+    this%current_mesh_iteration = this%current_mesh_iteration + 1
+  end subroutine eors_update_refinement_flags
+  
+  function eors_has_finished_refinement(this)
+    class(error_objective_refinement_strategy_t), intent(inout) :: this
+    logical :: eors_has_finished_refinement
+    real(rp), pointer :: local_estimate_entries(:)
+    real(rp)          :: max_local_estimate, min_local_estimate
+    real(rp)          :: error_upper_bound, error_lower_bound
+    local_estimate_entries => this%error_estimator%get_local_estimate_entries()
+    max_local_estimate = maxval(local_estimate_entries)
+    min_local_estimate = minval(local_estimate_entries)
+    error_upper_bound  = this%error_objective * ( 1.0_rp + this%objective_tolerance )
+    error_lower_bound  = this%error_objective * ( 1.0_rp - this%objective_tolerance )
+    eors_has_finished_refinement = ( ( ( max_local_estimate < error_upper_bound )  .and. &
+                                       ( min_local_estimate > error_lower_bound ) ) .or. & 
+                                     ( this%current_mesh_iteration > this%max_num_mesh_iterations ) )
+    if ( this%current_mesh_iteration > this%max_num_mesh_iterations ) &
+      write(*,*) 'Error objective mesh refinement strategy exceeded the maximum number of iterations'
+  end function eors_has_finished_refinement
+  
+end module refinement_strategy_names
