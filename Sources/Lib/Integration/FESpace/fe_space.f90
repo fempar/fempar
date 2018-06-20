f1871887289800f0a56e0270b6f421d42fff79ed
amartin <amartin@cimne.upc.edu>
2018-06-20 13:31:41 +0200

    * Fixed a BUG coming into experimental from merge request !230.   type(par_fe_space_t%update_hanging_dof_values(...) MUST exist and overwrite   its parent counterpart in order to do L1 MPI tasks dispatching.
    * Changed the implementation of type(serial_fe_space_t)%update_ghost_dof_values(...)
      s.t. it does nothing instead of raising an error condition.

--- Sources/Lib/Integration/FESpace/fe_space.f90
+++ Sources/Lib/Integration/FESpace/fe_space.f90
@@ -790,7 +790,7 @@ module fe_space_names
      generic                             :: refine_and_coarsen                            => serial_fe_space_refine_and_coarsen_single_fe_function, &
                                                                                              serial_fe_space_refine_and_coarsen_fe_function_array
 
-     procedure, non_overridable          :: update_hanging_dof_values                     => serial_fe_space_update_hanging_dof_values
+     procedure                           :: update_hanging_dof_values                     => serial_fe_space_update_hanging_dof_values
      procedure                           :: update_ghost_dof_values                       => serial_fe_space_update_ghost_dof_values
      
      
@@ -1021,8 +1021,9 @@ module fe_space_names
    ! Objects-related traversals
    procedure, non_overridable                  :: create_fe_object_iterator                       => par_fe_space_create_fe_object_iterator
    procedure, non_overridable                  :: free_fe_object_iterator                         => par_fe_space_free_fe_object_iterator
-   
-   procedure, non_overridable                  :: update_ghost_dof_values                         => par_fe_space_update_ghost_dof_values
+
+   procedure                                   :: update_hanging_dof_values                       => par_fe_space_update_hanging_dof_values
+   procedure                                   :: update_ghost_dof_values                         => par_fe_space_update_ghost_dof_values
    
    end type par_fe_space_t
  
