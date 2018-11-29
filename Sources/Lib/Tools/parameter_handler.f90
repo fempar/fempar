fempar/fempar

commit 8aba9f16d10f44e1e8c5bdeb77fa426a39401b1a

Merged into a single branch a miscellaneous of developments coming from fempar_parameter_handler, exploring_ford_documentation that are ready to be merged into experimental. Among these changes, an important one is that now we have added the num_dims member variable to data types in function.f90, so that subclasses are no longer reponsible for that.
**IMPORTANT NOTE**: be aware that your drivers wont compile after this
commit, but it can be fixed with very simple changes as the ones
performed in this commit within the Tests directory, that u can use for
reference.

Sources/Lib/Tools/parameter_handler.f90
@@ -153,7 +153,7 @@ contains
     implicit none
     class(parameter_handler_t), target , intent(in) :: this
     type(ParameterList_t)  , pointer    :: parameter_handler_get_required
-    parameter_handler_get_required => this%required
+    !parameter_handler_get_required => this%required
   end function parameter_handler_get_required
 
   !==================================================================================================
@@ -184,7 +184,7 @@ contains
      
      assert(this%switches%length() == this%switches_ab%length())
      assert(this%switches%length() == this%helpers%length())
-     assert(this%switches%length() == this%required%length())
+!     assert(this%switches%length() == this%required%length())
 !     assert(this%switches%length() == this%values%length())
      
      Iterator = this%switches%GetIterator()
@@ -192,7 +192,7 @@ contains
           key = Iterator%GetKey()
           assert(this%switches_ab%isPresent(key))
           assert(this%helpers%isPresent(key))
-          assert(this%required%isPresent(key))
+          !assert(this%required%isPresent(key))
           assert(this%values%isPresent(key))
           if (Iterator%isSubList()) then
             assert(Iterator%GetSublist(switches_sublist)==0)
@@ -295,7 +295,8 @@ contains
        error = error + Iterator%GetAsString   (switch)
        error = error + switches_ab%GetAsString(key = key , String = switch_ab)
        error = error + helpers%GetAsString    (key = key , String = help)
-       error = error + required%Get           (key = key , value = is_required)
+       !error = error + required%Get           (key = key , value = is_required)
+       is_required = .false.
        error = error + values%GetAsString     (key = key , string = cvalue, separator=" ")
        if(values%GetDimensions(Key=Iterator%GetKey()) == 0) then 
         call this%cli%add(group=group,switch=switch,switch_ab=switch_ab, help=help, &

Drivers/par_pb_bddc_vector_poisson/modules/vector_poisson_analytical_functions.f90
@@ -31,31 +31,18 @@ module vector_poisson_analytical_functions_names
   implicit none
 # include "debug.i90"
   private
-
-  type, extends(scalar_function_t) :: base_scalar_function_t
-    integer(ip) :: num_dims = -1  
-  contains
-    procedure :: set_num_dims    => base_scalar_function_set_num_dims
-  end type base_scalar_function_t
-  
-  type, extends(vector_function_t) :: base_vector_function_t
-    integer(ip) :: num_dims = -1  
-  contains
-    procedure :: set_num_dims    => base_vector_function_set_num_dims
-  end type base_vector_function_t
-  
-  
-  type, extends(base_vector_function_t) :: source_term_t
+    
+  type, extends(vector_function_t) :: source_term_t
    contains
      procedure :: get_value_space => source_term_get_value_space
   end type source_term_t
 
-  type, extends(base_scalar_function_t) :: boundary_function_t
+  type, extends(scalar_function_t) :: boundary_function_t
    contains
      procedure :: get_value_space => boundary_function_get_value_space  
   end type boundary_function_t
 
-  type, extends(base_vector_function_t) :: solution_function_t
+  type, extends(vector_function_t) :: solution_function_t
    contains
      procedure :: get_value_space    => solution_function_get_value_space
      procedure :: get_gradient_space => solution_function_get_gradient_space
@@ -77,29 +64,13 @@ module vector_poisson_analytical_functions_names
 
 contains  
 
-  subroutine base_scalar_function_set_num_dims ( this, num_dims )
-    implicit none
-    class(base_scalar_function_t), intent(inout)    :: this
-    integer(ip), intent(in) ::  num_dims
-    this%num_dims = num_dims
-  end subroutine base_scalar_function_set_num_dims
-
-  !===============================================================================================
-
-  subroutine base_vector_function_set_num_dims ( this, num_dims )
-    implicit none
-    class(base_vector_function_t), intent(inout)    :: this
-    integer(ip), intent(in) ::  num_dims
-    this%num_dims = num_dims
-  end subroutine base_vector_function_set_num_dims
-
   !===============================================================================================
   subroutine source_term_get_value_space ( this, point, result )
     implicit none
     class(source_term_t), intent(in)    :: this
     type(point_t)       , intent(in)    :: point
     type(vector_field_t), intent(inout) :: result
-    if ( this%num_dims == 2 ) then
+    if ( this%get_num_dims() == 2 ) then
       call result%set(1,0.0_rp)
       call result%set(2,0.0_rp) !2 * ( pi**2 ) * sin ( pi * point%get(1) ) * sin ( pi * point%get(2) )
     else
@@ -115,7 +86,7 @@ contains
     class(boundary_function_t), intent(in)    :: this
     type(point_t)           , intent(in)    :: point
     real(rp)                , intent(inout) :: result
-    if ( this%num_dims == 2 ) then
+    if ( this%get_num_dims() == 2 ) then
       result = point%get(1)+point%get(2) !sin ( pi * point%get(1) ) * sin ( pi * point%get(2) ) + point%get(1)
     else
       result = point%get(1)+point%get(2)+point%get(3)
@@ -128,7 +99,7 @@ contains
     class(solution_function_t), intent(in)    :: this
     type(point_t)           , intent(in)    :: point
     type(vector_field_t)    , intent(inout) :: result
-    if ( this%num_dims == 2 ) then
+    if ( this%get_num_dims() == 2 ) then
       call result%set(1, point%get(1)+point%get(2) ) 
       call result%set(2, point%get(1)+point%get(2) ) 
     else
@@ -144,7 +115,7 @@ contains
     class(solution_function_t), intent(in)    :: this
     type(point_t)             , intent(in)    :: point
     type(tensor_field_t)      , intent(inout) :: result
-    if ( this%num_dims == 2 ) then
+    if ( this%get_num_dims() == 2 ) then
       call result%set( 1, 1, 1.0_rp ) 
       call result%set( 2, 1, 1.0_rp )
       call result%set( 1, 2, 1.0_rp ) 
@@ -152,7 +123,7 @@ contains
     else
       call result%init(1.0_rp)
     end if
-    !if ( this%num_dims == 2 ) then
+    !if ( this%get_num_dims() == 2 ) then
     !  call result%set( 1, 1, 1.0_rp ) 
     !  call result%set( 2, 1, 0.0_rp )
     !  call result%set( 1, 2, 1.0_rp ) 

Drivers/test_mesh_gen/test_mesh_gen.f90
@@ -83,7 +83,7 @@ contains
     error = error + this%list%set(key = prefix_key                    , value = 'uniform')
     error = error + this%list%set(key = dir_path_out_key              , value = '.')
     error = error + this%list%set(key = num_dims_key      , value =  2)
-    error = error + this%list%set(key = interpolation_order_key       , value =  1)
+    error = error + this%list%set(key = geometric_interpolation_order_key       , value =  1)
     tmp = [4,4,0]; error = error + this%list%set(key = num_cells_x_dir_key   , value = tmp)
     tmp = [3,1,1]; error = error + this%list%set(key = num_parts_x_dir_key   , value = tmp)
     tmp = [0,1,0]; error = error + this%list%set(key = is_dir_periodic_key           , value = tmp)

Sources/Lib/Geometry/Tools/uniform_hex_mesh_generator.f90
@@ -40,7 +40,7 @@ module uniform_hex_mesh_generator_names
   character(len=*), parameter :: num_cells_x_dir_key = 'num_cells_x_dir'
   character(len=*), parameter :: num_parts_x_dir_key = 'num_parts_x_dir'
   character(len=*), parameter :: is_dir_periodic_key         = 'is_dir_periodic'
-  character(len=*), parameter :: interpolation_order_key     = 'interpolation_order'
+  character(len=*), parameter :: geometric_interpolation_order_key     = 'geometric_interpolation_order'
   character(len=*), parameter :: hex_mesh_domain_limits_key  = 'hex_mesh_domain_limits'
 
   public :: num_dims_key
@@ -48,7 +48,7 @@ module uniform_hex_mesh_generator_names
   public :: num_cells_x_dir_key 
   public :: num_parts_x_dir_key 
   public :: is_dir_periodic_key         
-  public :: interpolation_order_key
+  public :: geometric_interpolation_order_key
   public :: hex_mesh_domain_limits_key
 
   integer(ip) , parameter :: not_described = 0

Sources/Lib/Integration/FESpace/fe_space.f90
@@ -1179,10 +1179,8 @@ module fe_space_names
   
   type, extends(vector_function_t) :: rigid_body_mode_t
     private
-    integer(ip)  :: num_dims = -1
     integer(ip)  :: imode = -1  
   contains
-    procedure :: set_num_dims         => rigid_body_mode_set_num_dims
     procedure :: set_mode             => rigid_body_mode_set_mode
     procedure :: get_values_set_space => rigid_body_mode_get_values_set_space 
   end type rigid_body_mode_t

Sources/Lib/Integration/FESpace/sbm_elasticity_coarse_fe_handler.i90
@@ -25,15 +25,6 @@
 ! resulting work. 
 !
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-subroutine rigid_body_mode_set_num_dims(this,num_dims)
-  implicit none
-  class(rigid_body_mode_t), intent(inout)  :: this
-  integer(ip), intent(in) ::  num_dims
-
-  this%num_dims = num_dims
-
-end subroutine rigid_body_mode_set_num_dims
-
 subroutine rigid_body_mode_set_mode(this,imode)
   implicit none
   class(rigid_body_mode_t), intent(inout)  :: this
@@ -48,31 +39,31 @@ subroutine rigid_body_mode_get_values_set_space( this, point, result )
   class(rigid_body_mode_t), intent(in)   :: this
   type(point_t), intent(in)            :: point(:)
   type(vector_field_t), intent(inout)  :: result(:)
-  integer(ip) :: i, num_points
+  integer(ip) :: i, num_points, num_dims
 
   num_points = size(point)
-
+  num_dims = this%get_num_dims()
   ! Translation modes
-  if (this%imode <= this%num_dims) then
+  if (this%imode <= num_dims) then
      do i = 1, num_points
         call result(i)%init(0.0_rp)
         call result(i)%set(this%imode,1.0_rp)
      end do
 
      ! Rotation modes
-  else if (this%imode == this%num_dims + 1) then ! r4 = [x2 -x1 0 ]^T
+  else if (this%imode == num_dims + 1) then ! r4 = [x2 -x1 0 ]^T
      do i = 1, num_points
         call result(i)%init(0.0_rp)
         call result(i)%set(1,point(i)%get(2))
         call result(i)%set(2,-point(i)%get(1))
      end do
-  else if (this%imode == this%num_dims + 2) then ! r5 = [-x3 0 x1 ]^T
+  else if (this%imode == num_dims + 2) then ! r5 = [-x3 0 x1 ]^T
      do i = 1, num_points
         call result(i)%init(0.0_rp)
         call result(i)%set(1,-point(i)%get(3))
         call result(i)%set(3,point(i)%get(1))
      end do
-  else if (this%imode == this%num_dims + 3) then ! r6 = [0 x3 -x2 ]^T
+  else if (this%imode == num_dims + 3) then ! r6 = [0 x3 -x2 ]^T
      do i = 1, num_points
         call result(i)%init(0.0_rp)
         call result(i)%set(2,point(i)%get(3))

Sources/Lib/Integration/Function/function.f90
@@ -36,9 +36,12 @@ module function_names
   type :: array_function_t
      private
      integer(ip)  :: num_components = 1
+     integer(ip)  :: num_dims = -1
    contains
      procedure, non_overridable :: set_num_components  => array_set_num_components
      procedure, non_overridable :: get_num_components  => array_function_get_num_components
+     procedure                  :: set_num_dims        => array_function_set_num_dims
+     procedure                  :: get_num_dims        => array_function_get_num_dims
      procedure  :: get_component_value_space              => array_function_get_component_value_space
      procedure  :: get_component_value_space_time         => array_function_get_component_value_space_time
      generic    :: get_component_value                    => get_component_value_space, get_component_value_space_time
@@ -55,7 +58,10 @@ module function_names
 
   type :: scalar_function_t
      private
+     integer(ip)  :: num_dims = -1
    contains
+     procedure                  :: set_num_dims                 => scalar_function_set_num_dims
+     procedure                  :: get_num_dims                 => scalar_function_get_num_dims
      procedure                  :: get_value_space              => scalar_function_get_value_space
      procedure                  :: get_value_space_time         => scalar_function_get_value_space_time
      generic                    :: get_value                    => get_value_space, get_value_space_time
@@ -73,8 +79,6 @@ module function_names
   
      procedure                  :: get_value_temporal_derivative      => scalar_function_get_value_temporal_derivative
      procedure                  :: get_values_set_temporal_derivative => scalar_function_get_values_set_temporal_derivative
-     
-     
   end type scalar_function_t
   
   type :: p_scalar_function_t
@@ -83,7 +87,10 @@ module function_names
 
   type :: vector_function_t
      private
+     integer(ip)  :: num_dims = -1
    contains
+     procedure                  :: set_num_dims                 => vector_function_set_num_dims
+     procedure                  :: get_num_dims                 => vector_function_get_num_dims
      procedure                  :: get_value_space              => vector_function_get_value_space
      procedure                  :: get_value_space_time         => vector_function_get_value_space_time
      generic                    :: get_value                    => get_value_space, get_value_space_time
@@ -105,7 +112,10 @@ module function_names
   
   type :: tensor_function_t
      private
+     integer(ip)  :: num_dims = -1
    contains
+     procedure                  :: set_num_dims              => tensor_function_set_num_dims
+     procedure                  :: get_num_dims              => tensor_function_get_num_dims
      procedure                  :: get_value_space           => tensor_function_get_value_space
      procedure                  :: get_value_space_time      => tensor_function_get_value_space_time
      generic                    :: get_value                 => get_value_space, get_value_space_time

Sources/Lib/Integration/Function/sbm_array_function.i90
@@ -38,6 +38,20 @@ function array_function_get_num_components( this )
   array_function_get_num_components = this%num_components
 end function array_function_get_num_components
 
+subroutine array_function_set_num_dims ( this, num_dims )
+  implicit none
+  class(array_function_t), intent(inout)    :: this
+  integer(ip)             , intent(in)       ::  num_dims
+  this%num_dims = num_dims
+end subroutine array_function_set_num_dims
+  
+function array_function_get_num_dims ( this )
+  implicit none
+  class(array_function_t), intent(in)    :: this
+  integer(ip)  ::  array_function_get_num_dims
+  array_function_get_num_dims = this%num_dims
+end function array_function_get_num_dims
+
 subroutine array_function_get_component_value_space( this, point, component, result )
   class(array_function_t), intent(in) :: this
   type(point_t), intent(in)          :: point

commit b4392e0ce1a425368c525a0020f75c1645bd16cd

Solved a BUG introduced in 8aba9f16d10f44e1e8c5bdeb77fa426a39401b1a. The body of the parameter_handler_t%get_required() was commented out and it should not if we want the tests to still pass.

Sources/Lib/Tools/parameter_handler.f90
@@ -153,7 +153,7 @@ contains
     implicit none
     class(parameter_handler_t), target , intent(in) :: this
     type(ParameterList_t)  , pointer    :: parameter_handler_get_required
-    !parameter_handler_get_required => this%required
+    parameter_handler_get_required => this%required
   end function parameter_handler_get_required
 
   !==================================================================================================