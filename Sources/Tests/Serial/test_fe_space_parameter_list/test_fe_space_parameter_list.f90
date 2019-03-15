3e0d100330f4bd9f2e2be7fd3854e2a9716ffafe
dgarcial <dagarcialhit@gmail.com>
2019-03-15 16:34:10 +0100

    Removing unnecessary lines, fixing the correct matrix type for pardiso and introducing parameter to allow switching between direct and iterative solvers

--- Sources/Tests/Serial/test_fe_space_parameter_list/test_fe_space_parameter_list.f90
+++ Sources/Tests/Serial/test_fe_space_parameter_list/test_fe_space_parameter_list.f90
@@ -40,7 +40,7 @@ program test_steady_poisson
   !* The parameter_handler is an object that provides default values for all the keys that FEMPAR uses to run. They can be:
   !*   1) Default values of the library.
   !*   2) Values provided by the user through the command line (using the keys in fempar_parameter_handler_t3).
-  !*   3) overwrites 2) which overwrites 1). 
+  !*   3) the user provides the parameter values in the code.
   !* In this tutorial we will explicitly provide the values in the code (option 3), but they could be provided by the command line argument instead.
   type(fempar_parameter_handler_t)     :: parameter_handler
   !* This is the object in parameter_handler_t that provides the list of parameters.
@@ -50,12 +50,12 @@ program test_steady_poisson
   !* This is a pointer to the finite element used to map the reference cell to the physical space. Usually, it is first order.
   !* The FE space used for this purpose will be extracted from the triangulation.
   class(reference_fe_t), pointer       :: reference_fe_geo
-  !* This is a pointer to the reference finite element used to approximate the Poisson problem.
-  type(p_reference_fe_t)               :: reference_fe
-  type(p_reference_fe_t), allocatable  :: reference_fes(:) 
-  !* The triangulation can provide a reference_fe... too complicated.
-  !* SB: Provide the topology in a different way!!!!
-  class(cell_iterator_t), allocatable  :: cell
+!!!!!  !* This is a pointer to the reference finite element used to approximate the Poisson problem.
+!!!!!  type(p_reference_fe_t)               :: reference_fe
+!!!!!  type(p_reference_fe_t), allocatable  :: reference_fes(:) 
+!!!!!  !* The triangulation can provide a reference_fe... too complicated.
+!!!!!  !* SB: Provide the topology in a different way!!!!
+!!!!!  class(cell_iterator_t), allocatable  :: cell
   !* The fe_space_t is the global finite element space to be used.
   type(serial_fe_space_t)              :: fe_space
   !* It is an extension of conditions_t that defines the Dirichlet boundary conditions using analytical functions.
@@ -73,18 +73,14 @@ program test_steady_poisson
   !* A fe_affine_operator_t that represents the affine operator the solution of which is the one we want, i.e., B = Ax-f.
   !* The solution is the root of this operator.
   type(fe_affine_operator_t)           :: fe_affine_operator
-  !* The problem ca be solved using a direct solver (Pardiso) or an iterative linear solver (fempar cg). The solvers and their 
+  !* The problem can be solved using a direct solver (Pardiso) or an iterative linear solver (fempar cg). The solvers and their 
   !* parameters are defined later.
-#ifdef ENABLE_MKL    
   !*  - Direct solver
   type(direct_solver_t)                :: direct_solver
   type(ParameterList_t), pointer       :: direct_solver_params
-  integer                              :: iparm(64) = 0  
-#else 
   !*  - Iterative solver
   type(iterative_linear_solver_t)      :: iterative_linear_solver
-  type(ParameterList_t), pointer       :: iterative_solver_params
-#endif    
+  type(ParameterList_t), pointer       :: iterative_solver_params    
   !* The output handler type is used to print the results
   type(output_handler_t) :: output_handler
   !* The following object automatically compute error norms given a fe_function_t and the analytical solution.
@@ -92,7 +88,8 @@ program test_steady_poisson
   !*
   !* Local variables
   integer(ip) :: fe_order, istat, error, i, boundary_ids
-  class(vector_t), pointer       :: dof_values
+  character(len=16)            :: lin_solver_type
+  class(vector_t), pointer     :: dof_values
   class(vector_t), allocatable :: rhs
   real(rp) :: l2
   !*
@@ -110,7 +107,7 @@ program test_steady_poisson
   !* Set parameters for the integration
   error = 0
   error = error + parameter_list%set(key = struct_hex_triang_num_dims_key, value = 2 )              ! Number of space dimensions
-  error = error + parameter_list%set(key = struct_hex_triang_num_cells_dir, value = [10,10,10] ) ! Number of cells per each dimension
+  error = error + parameter_list%set(key = struct_hex_triang_num_cells_dir, value = [10,10,10] )    ! Number of cells per each dimension
   error = error + parameter_list%set(key = fes_ref_fe_orders_key, value = [ 1 ] )                   ! Reference finite element orders
   error = error + parameter_list%set(key = fes_ref_fe_types_key, value = 'Lagrangian' )             ! Reference finite element types
   mcheck(error==0,'Failed parameter set')
@@ -122,9 +119,7 @@ program test_steady_poisson
   !* The structure mesh generator creates a domain that has:
   !*   8 boundary objects (corners+edges) in 2D
   !*   26 boundary objects (face+corners+edges) in 3D
-  !* and 1 interior object 
-  !*   1 face in 2D 
-  !*   1 cell in 3D
+  !* and 1 interior object  (i.e., the cell interior).
   !* A graphical example for a 2D case is presented next
   !*           6
   !*      4 - - - - 3
@@ -170,62 +165,62 @@ program test_steady_poisson
   call poisson_integration%set_fe_function(solution)
   !* Now, the discrete integration has all the information needed. We can fill the affine operator b - Ax.
   call fe_affine_operator%compute()
-#ifdef ENABLE_MKL
-  !* PARDISO MKL
-  direct_solver_params => parameter_list%NewSubList(Key=pardiso_mkl)
-  !* Set parameters of the direct solver
-  error = 0
-  error = error + direct_solver_params%set(key = dls_type_key,        value = pardiso_mkl)
-  error = error + direct_solver_params%set(key = pardiso_mkl_matrix_type,   value = pardiso_mkl_uss)
-  error = error + direct_solver_params%set(key = pardiso_mkl_message_level, value = 0)
-  error = error + direct_solver_params%set(key = pardiso_mkl_iparm,         value = iparm)
-  check(error == 0)
-  call direct_solver%set_type_from_pl(direct_solver_params)
-  !* Next, we set the matrix in our system from the fe_affine operator
-  call direct_solver%set_matrix(fe_affine_operator%get_matrix())
-  !* We extract a pointer to the free nodal values of our FE function, which is the plae in which we will store the result.
-  dof_values => solution%get_free_dof_values()
-  !* We solve the problem with the matrix already associated, the RHS obtained from the fe_affine_operator using get_translation, and 
-  !* putting the result in dof_values.
-  call direct_solver%solve(fe_affine_operator%get_translation(),dof_values)
-  call direct_solver%log_info()
-#else   
-  !* ITERATIVE SOLVER
-  iterative_solver_params => parameter_list%NewSubList(Key=cg_solver)
-  !* Set parameters of the direct solver
-  error = 0
-  error = error + iterative_solver_params%set(key = ils_rtol_key, value = 1.0e-12_rp)         ! Relative tolerance
-  error = error + iterative_solver_params%set(key = ils_max_num_iterations_key, value = 5000) ! Max number of iterations
-  error = error + iterative_solver_params%set(key = ils_type_key, value = cg_name )           ! Conjugate Gradient iterative solver
-  assert(error == 0)
-  !* Now, we create a serial iterative solver with the values in the parameter list.
-  call iterative_linear_solver%create(fe_space%get_environment())
-  call iterative_linear_solver%set_type_and_parameters_from_pl(iterative_solver_params)
-  !* Next, we set the matrix in our system from the fe_affine operator (i.e., its tangent)
-  call iterative_linear_solver%set_operators(fe_affine_operator%get_tangent(), .identity. fe_affine_operator) 
-  !* We extract a pointer to the free nodal values of our FE function, which is the plae in which we will store the result.
-  dof_values => solution%get_free_dof_values()
-  !* We solve the problem with the matrix already associated, the RHS obtained from the fe_affine_operator using get_translation, and 
-  !* putting the result in dof_values.
-  call iterative_linear_solver%apply(fe_affine_operator%get_translation(),dof_values)   
-#endif  
+  !* After, we select the linear solver type. Then we set the solver parameters and solve the problem.
+  lin_solver_type='pardiso'
+  !*
+  if (lin_solver_type=='pardiso') then
+    !* PARDISO MKL
+    direct_solver_params => parameter_list%NewSubList(Key=pardiso_mkl)
+    !* Set parameters of the direct solver
+    error = 0
+    error = error + direct_solver_params%set(key = dls_type_key,        value = pardiso_mkl)
+    error = error + direct_solver_params%set(key = pardiso_mkl_matrix_type,   value = pardiso_mkl_spd)
+    error = error + direct_solver_params%set(key = pardiso_mkl_message_level, value = 0)
+    check(error == 0)
+    call direct_solver%set_type_from_pl(direct_solver_params)
+    !* Next, we set the matrix in our system from the fe_affine operator
+    call direct_solver%set_matrix(fe_affine_operator%get_matrix())
+    !* We extract a pointer to the free nodal values of our FE function, which is the plae in which we will store the result.
+    dof_values => solution%get_free_dof_values()
+    !* We solve the problem with the matrix already associated, the RHS obtained from the fe_affine_operator using get_translation, and 
+    !* putting the result in dof_values.
+    call direct_solver%solve(fe_affine_operator%get_translation(),dof_values)
+    call direct_solver%log_info()
+  else if (lin_solver_type=='cg_solver') then
+    !* ITERATIVE SOLVER
+    iterative_solver_params => parameter_list%NewSubList(Key=cg_name)
+    !* Set parameters of the direct solver
+    error = 0
+    error = error + iterative_solver_params%set(key = ils_rtol_key, value = 1.0e-12_rp)         ! Relative tolerance
+    error = error + iterative_solver_params%set(key = ils_max_num_iterations_key, value = 5000) ! Max number of iterations
+    error = error + iterative_solver_params%set(key = ils_type_key, value = cg_name )           ! Conjugate Gradient iterative solver
+    assert(error == 0)
+    !* Now, we create a serial iterative solver with the values in the parameter list.
+    call iterative_linear_solver%create(fe_space%get_environment())
+    call iterative_linear_solver%set_type_and_parameters_from_pl(iterative_solver_params)
+    !* Next, we set the matrix in our system from the fe_affine operator (i.e., its tangent)
+    call iterative_linear_solver%set_operators(fe_affine_operator%get_tangent(), .identity. fe_affine_operator) 
+    !* We extract a pointer to the free nodal values of our FE function, which is the plae in which we will store the result.
+    dof_values => solution%get_free_dof_values()
+    !* We solve the problem with the matrix already associated, the RHS obtained from the fe_affine_operator using get_translation, and 
+    !* putting the result in dof_values.
+    call iterative_linear_solver%apply(fe_affine_operator%get_translation(),dof_values)    
+  end if 
   !* Now, we want to compute error wrt an exact solution. It needs the FE space above to be constructed.                                       
   call error_norm%create(fe_space,1)  
   call exact_solution%set_num_dims(triangulation%get_num_dims())
   !* We compute the L2 norm of the difference between the exact and computed solution.
   l2 = error_norm%compute(exact_solution, solution, l2_norm) 
-  !* We finally write the result and check we have solved the problem "exactly", since the exact solution belongs to the FE space.
+  !* We finally plot the result and check we have solved the problem "exactly", since the exact solution belongs to the FE space.
   write(*,'(a20,e32.25)') 'l2_norm:', l2; check ( l2 < 1.0e-04 )
   !* Last, we plot the results.
-  !* We can set the direction where the results files are created using
-  !*   error = 0
-  !*   error = error + direct_solver_params%set(key = dir_path_out_key, value = '.')
-  !*   check(error == 0)
-  !* But in this example we use the default path.
+  !* We can set the file system path where the results files are created using
+  error = 0
   error = error + parameter_list%set(key = dir_path_out_key      , Value= 'RESULTS_PROBLEM')
+  assert(error == 0)
   call output_handler%create()
   call output_handler%attach_fe_space(fe_space)
-  call output_handler%add_fe_function(solution, 1, 'phi')
+  call output_handler%add_fe_function(solution, 1, 'solution')
   call output_handler%open(parameter_handler%get_dir_path_out(), parameter_handler%get_prefix())
   call output_handler%write()
   call output_handler%close()
@@ -235,12 +230,9 @@ program test_steady_poisson
 !*  call fempar_parameter_handler%free()
 !*  call triangulation%free()
   call error_norm%free()
-  call solution%free()
-#ifdef ENABLE_MKL        
-    call direct_solver%free()
-#else
-    call iterative_linear_solver%free()
-#endif
+  call solution%free()       
+  call direct_solver%free()
+  call iterative_linear_solver%free()
   call fe_affine_operator%free()
   call fe_space%free()
   call poisson_conditions%free()
