ec46cda7645d9a9abf9d08b796fb539e307bfd00
victorsndvg <victorsv@gmail.com>
2019-03-20 10:49:53 +0100

    Update par_test_transient_poisson driver and tests with fempar_parameters

--- Sources/Tests/Par/par_test_poisson_unfitted/modules/par_test_poisson_unfitted_driver.f90
+++ Sources/Tests/Par/par_test_poisson_unfitted/modules/par_test_poisson_unfitted_driver.f90
@@ -132,7 +132,7 @@ contains
   subroutine parse_command_line_parameters(this)
     implicit none
     class(par_test_poisson_unfitted_fe_driver_t), intent(inout) :: this
-    call this%test_params%create()
+    call this%test_params%process_parameters()
     !call this%test_params%parse(this%parameter_list)
     this%parameter_list => this%test_params%get_values()
   end subroutine parse_command_line_parameters
