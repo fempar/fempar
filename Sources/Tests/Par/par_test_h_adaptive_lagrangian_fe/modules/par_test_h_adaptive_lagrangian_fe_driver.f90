07e15a75a0c590060ce43a82ea099c45cfa60584
victorsndvg <victorsv@gmail.com>
2019-03-21 17:25:28 +0100

    Update par_test_h_adaptive_lagrangian_fe driver and tests with fempar_parameters

--- Sources/Tests/Par/par_test_h_adaptive_lagrangian_fe/modules/par_test_h_adaptive_lagrangian_fe_driver.f90
+++ Sources/Tests/Par/par_test_h_adaptive_lagrangian_fe/modules/par_test_h_adaptive_lagrangian_fe_driver.f90
@@ -121,7 +121,7 @@ contains
   subroutine parse_command_line_parameters(this)
     implicit none
     class(par_test_h_adaptive_lagrangian_fe_driver_t), intent(inout) :: this
-    call this%test_params%create()
+    call this%test_params%process_parameters()
     this%parameter_list => this%test_params%get_values()
   end subroutine parse_command_line_parameters
 
