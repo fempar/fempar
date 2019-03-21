a7c3b5734c712cbec817ecaf981017038a9a7f76
victorsndvg <victorsv@gmail.com>
2019-03-21 13:43:58 +0100

    Update par_test_nsi driver and tests with fempar_parameters

--- Sources/Tests/Par/par_test_nsi/modules/par_test_nsi_driver.f90
+++ Sources/Tests/Par/par_test_nsi/modules/par_test_nsi_driver.f90
@@ -177,7 +177,7 @@ contains
   subroutine parse_command_line_parameters(this)
     implicit none
     class(par_test_nsi_fe_driver_t), intent(inout) :: this
-    call this%test_params%create()
+    call this%test_params%process_parameters()
     this%parameter_list => this%test_params%get_values()
   end subroutine parse_command_line_parameters
 
