c91204f3a0a95db40c33a25df8c645e89295ba29
amartin <amartin@cimne.upc.edu>
2019-06-03 17:21:32 +0200

    Cambios para hacer commit: 	deleted:    Sources/Tests/Scripts/renaming.sh

--- Sources/Tests/Scripts/renaming.sh
+++
@@ -1,9 +0,0 @@
-
-
-# for i in $(find . -name *.[if]90); do grep femap $i; done
-before=$1
-after=$2
-
-# Test scripts
-cd ~/git-repos/fempar/Sources/Tests/Scripts
-for i in $(ls); do j=$(cat $i |  grep $before );  if [ ! "$j" == "" ]; then cat $i | sed s/"$before"/"$after"/g > /tmp/kk; cp /tmp/kk $i; fi; done;
